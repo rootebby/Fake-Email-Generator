37ef92ba236aeed124ad835082ec40186b216016
rootebby <55960000+rootebby@users.noreply.github.com>
2020-08-15 20:40:59 +0300

    Update FakeRandomEmails.py

--- FakeRandomEmails.py
+++ FakeRandomEmails.py
@@ -3,10 +3,7 @@ import string
 import csv
 import progressbar
 
-'''
-Asks user for how many fake emails they want generated. Must be Integer. Duh.
-If not an integer, keeps recursively cycling back until it gets an integer. 
-'''
+
 def getcount():
 	rownums = input("How many fake email addresses?: ")
 	try:
@@ -16,10 +13,7 @@ def getcount():
 		print ("Please enter an integer value")
 		return getcount()
 
-'''
-Creates a random string of digits between 1 and 20 characters alphanumeric and adds it to a fake domain and fake extension
-Most of these emails are completely bogus (eg - gmail.gov) but will meet formatting requirements for my purposes
-'''
+
 def makeEmail():
 	extensions = ['com','net','org','gov']
 	domains = ['gmail','yahoo','comcast','verizon','charter','hotmail','outlook','frontier']
@@ -34,16 +28,12 @@ def makeEmail():
 	finale = (winacc + "@" + windom + "." + winext)
 	return finale
 
-#save count to var
 howmany = getcount()
 
-#counter for While loop
 counter = 0
 
-#empty array for loop
 emailarray = []
 
-#uses counter to figure out how many emails to keep making
 
 print ("Creating email addresses...")
 print ("Progress: ")
@@ -58,15 +48,9 @@ for i in prebar(range(howmany)):
 	
 print ("Creation completed.")
 
-#user input for filename. Can even take in a file path, pending you have permission to write there
 filename = input("name your file: ")
 
-'''
-Acutally writes to file here.
-uses WA which will write and append the file. If the file doesn't exist, this will create it. 
-Will write email addresses to array one per row which is why the array is important. 
-Once complete, will close the file. 
-'''
+
 print ("Progress: ")
 
 bar = progressbar.ProgressBar(maxval=int(howmany))
